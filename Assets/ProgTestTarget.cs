8d5a11ff0ed5405bb4a60f78f00ebf853480aba1
HenryB123 <henry44magnum@live.com>
2020-09-22 23:30:42 -0500

    Rail game, made Follow and ShipMove scripts

---
+++ Assets/ProgTestTarget.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgTestTarget : MonoBehaviour
+{
+    progTestScoreKeeper scoreKeeper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreKeeper = GameObject.Find("ScoreKeeper").GetComponent<progTestScoreKeeper>();
+        scoreKeeper.UpdateScore(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // void OnTriggerEnter() that adds 100 points to score.
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            scoreKeeper.UpdateScore(100);
+
+        }
+    }
+    
+    //in Start():   StartCoroutine(FindScoreKeeper());
+    IEnumerator FindScoreKeeper()
+    {
+        float counter = 0;
+        while(counter < 5)
+        {
+            if(GameObject.Find("ScoreKeeper").GetComponent<progTestScoreKeeper>() != null)
+            {
+                //check to see if scoreKeeper is equal to anything
+                if(scoreKeeper != null)
+                {
+                Debug.Log("ScoreKeeper = " + scoreKeeper);
+                scoreKeeper.UpdateScore(1);
+                scoreKeeper.UpdateScore(-5);
+                //break out of the loop and consequently, the FindScoreKeeper() method.
+                break;
+                }
+            }
+
+            
+            
+
+            //wait for 0.1 sconds and try again.
+            counter += 0.1f;
+            Debug.Log("I couldn't find scorekeeper, trying again...");
+            yield return new WaitForSeconds(0.1f);
+        }//end of while loop
+    }
+}
---
+++ Assets/code/ShipMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShipMove : MonoBehaviour
+{
+    // Move object using accelerometer
+    public float speed = 10.0f;
+    public float rotSpeed = 60f;
+
+    //Transform body;
+    void Start()
+    {
+        // "body" variable is equal to the "Body" object in the Player object
+        //body = this.transform.GetChild(0);
+    }
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        // we assume that device is held parallel to the ground
+        // and Home button is in the right hand
+
+        // remap device acceleration axis to game coordinates:
+        //  1) XY plane of the device is mapped onto XZ plane
+        //  2) rotated 90 degrees around Y axis
+        dir.x = Input.acceleration.x;
+        dir.z = Input.acceleration.y;
+
+        // clamp acceleration vector to unit sphere
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        dir *= Time.deltaTime;
+
+        // Move object
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);      // move forward at 10m per second.
+        transform.Translate(dir * speed);                                       // move left/right and forward/back.
+        transform.Rotate(0,0,-dir.x * rotSpeed);                           // rotate the body around the z-axis.
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Respawn"))
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
